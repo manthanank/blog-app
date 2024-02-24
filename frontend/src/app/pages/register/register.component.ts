91f62047920fe8146735f12d695d64d55f30d1e4
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-02-24 19:40:58 +0530

    Update register component with new form and styling

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RegisterService } from './register.service';
+import { Router, RouterLink } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, HttpClientModule, RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  providers: [RegisterService],
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  authService = inject(RegisterService);
+  router = inject(Router);
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.authService
+      .register(this.registerForm.value.email, this.registerForm.value.password)
+      .subscribe((data: any) => {
+        if (data.message) {
+          this.router.navigate(['/home']);
+          // localStorage.setItem('token', data.token);
+        }
+      });
+  }
 }
