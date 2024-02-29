0925f606d7b05cd4e67617b852ed2600037021d0
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-03-01 00:17:26 +0530

    Handle expired JWT tokens in auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -14,6 +14,9 @@ module.exports = (req, res, next) => {
         req.user = decoded;
         next();
     } catch (err) {
+        if (err instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ message: 'Token expired' });
+        }
         res.status(403).json({ message: 'Invalid token' });
     }
 };
