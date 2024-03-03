76b3e8b79be2c3f76a828b8f23edb81f29635492
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-03-03 17:44:40 +0530

    Add routes for blog functionality

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -3,16 +3,74 @@ const router = express.Router();
 const blogController = require('../controllers/blogController');
 const auth = require('../middlewares/auth');
 
+/**
+ * @route GET /blogs
+ * @desc Get all posts
+ * @access Public
+ */
 router.get('/blogs', blogController.getAllPosts);
+
+/**
+ * @route GET /blogs/featured
+ * @desc Get featured posts
+ * @access Public
+ */
 router.get('/blogs/featured', blogController.getFeaturedPosts);
+
+/**
+ * @route GET /blogs/recent
+ * @desc Get recent posts
+ * @access Public
+ */
 router.get('/blogs/recent', blogController.getRecentPosts);
+
+/**
+ * @route GET /blogs/tags
+ * @desc Get all tags
+ * @access Public
+ */
 router.get('/blogs/tags', blogController.getAllTags);
+
+/**
+ * @route GET /blogs/:id
+ * @desc Get a post by ID
+ * @access Public
+ */
 router.get('/blogs/:id', blogController.getPostById);
+
+/**
+ * @route GET /blogs/tags/:tag
+ * @desc Get posts by tag
+ * @access Public
+ */
 router.get('/blogs/tags/:tag', blogController.getPostsByTag);
-// add search route
+
+/**
+ * @route GET /blogs/search/:query
+ * @desc Search posts by query
+ * @access Public
+ */
 router.get('/blogs/search/:query', blogController.searchPosts);
+
+/**
+ * @route POST /blogs
+ * @desc Create a new post
+ * @access Private
+ */
 router.post('/blogs', auth, blogController.createPost);
+
+/**
+ * @route PUT /blogs/:id
+ * @desc Update a post by ID
+ * @access Private
+ */
 router.put('/blogs/:id', auth, blogController.updatePost);
+
+/**
+ * @route DELETE /blogs/:id
+ * @desc Delete a post by ID
+ * @access Private
+ */
 router.delete('/blogs/:id', auth, blogController.deletePost);
 
 module.exports = router;
