2c9106b39fa25fea75a3dc9e73db611bf465a024
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-03-06 20:58:08 +0530

    Add @ngrx/effects and @ngrx/store dependencies

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch(), withInterceptors([authInterceptor]))]
+  providers: [provideRouter(routes), provideHttpClient(withFetch(), withInterceptors([authInterceptor])), provideStore(), provideEffects()]
 };
