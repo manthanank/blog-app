6c027a9988158b37137b40bb57dc27d91d3108d8
manthanank <manthan.ank46@gmail.com>
2024-03-17 19:42:24 +0530

    ngrx removed

--- src/app/store/blogs.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as fromBlogs from './blogs.reducer';
-
-export const selectBlogsState = createFeatureSelector<fromBlogs.State>('blogs');
-
-export const selectAllBlogs = createSelector(
-    selectBlogsState,
-    state => state.blogs
-);
