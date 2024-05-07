5f828f4c97ec81fa00b2359fd11a9853a34ecc07
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-05-07 10:49:08 +0530

    Refactor environment.development.ts to update API URL for deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://light-duck-crown.cyclic.app/api'
+    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://blog-app-nh06.onrender.com/api'
 };
