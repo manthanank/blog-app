15b6e33030edb7f69674126c7ce77fe220111f1d
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-05-09 10:52:25 +0530

    Refactor blog-details.component.html to use readonly textarea and adjust line height dynamically

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -76,18 +76,6 @@ exports.getPostById = async (req, res) => {
     }
 };
 
-// Get blog posts by author ID
-exports.getPostsByAuthor = async (req, res) => {
-    try {
-        const authorId = req.params.authorId;
-        const posts = await BlogPost.find({ authorId: authorId });
-        res.json(posts);
-    }
-    catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-}
-
 // Add search route
 exports.searchPosts = async (req, res) => {
     try {
@@ -101,27 +89,6 @@ exports.searchPosts = async (req, res) => {
 
 // Create a new blog post
 exports.createPost = async (req, res) => {
-    const post = new BlogPost({
-        slug: req.body.title.toLowerCase().split(' ').join('-'),
-        title: req.body.title,
-        desc: req.body.desc,
-        content: req.body.content,
-        author: req.body.author,
-        tags: req.body.tags,
-        createdAt: req.body.createdAt,
-        featured: req.body.featured,
-        authorId: req.body.authorId
-    });
-    try {
-        const newPost = await post.save();
-        res.status(201).json(newPost);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-};
-
-// Create a new blog post by username
-exports.createPostByUsername = async (req, res) => {
     const post = new BlogPost({
         slug: req.body.title.toLowerCase().split(' ').join('-'),
         title: req.body.title,
@@ -168,33 +135,6 @@ exports.updatePost = async (req, res) => {
     }
 };
 
-// Update a blog post by username
-exports.updatePostByUsername = async (req, res) => {
-    try {
-        const post = await BlogPost.findOne({ slug: req.params.slug });
-        if (!post) {
-            return res.status(404).json({ message: 'Blog post not found' });
-        }
-        
-        // Check if the current user is the creator of the post
-        if (post.author !== req.user.username) {
-            return res.status(403).json({ message: 'You are not authorized to update this post' });
-        }
-        
-        post.slug = req.body.title.toLowerCase().split(' ').join('-');
-        post.title = req.body.title;
-        post.desc = req.body.desc;
-        post.content = req.body.content;
-        post.tags = req.body.tags;
-        post.featured = req.body.featured;
-        
-        const updatedPost = await post.save();
-        res.json(updatedPost);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-};
-
 // Delete a blog post
 exports.deletePost = async (req, res) => {
     try {
@@ -203,20 +143,6 @@ exports.deletePost = async (req, res) => {
             return res.status(404).json({ message: 'Blog post not found' });
         }
         await BlogPost.deleteOne({ _id: req.params.id }); // Use deleteOne method to delete the blog post
-        res.json({ message: 'Blog post deleted' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
-
-// Delete a blog post by username
-exports.deletePostByUsername = async (req, res) => {
-    try {
-        const post = await BlogPost.findOne({ slug: req.params.slug });
-        if (!post) {
-            return res.status(404).json({ message: 'Blog post not found' });
-        }
-        await BlogPost.deleteOne({ slug: req.params.slug }); // Use deleteOne method to delete the blog post
         res.json({ message: 'Blog post deleted' });
     } catch (err) {
         res.status(500).json({ message: err.message });
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -8,13 +8,9 @@ router.get('/blogs/latest', blogController.getLatestPosts);
 router.get('/blogs/:id', blogController.getPostById);
 router.get('/blogs/all/:username', blogController.getAllPostsByUsername);
 router.get('/blogs/featured/:username', blogController.getFeaturedPostsByUsername);
-router.get('/blogs/author/:authorId', blogController.getPostsByAuthor);
 router.get('/blogs/search', blogController.searchPosts);
 router.post('/blogs', auth, blogController.createPost);
-router.post('/blogs/:username', auth, blogController.createPostByUsername);
 router.put('/blogs/:id', auth, blogController.updatePost);
-router.put('/blogs/:username', auth, blogController.updatePostByUsername);
 router.delete('/blogs/:id', auth, blogController.deletePost);
-router.delete('/blogs/:username', auth, blogController.deletePostByUsername);
 
 module.exports = router;
